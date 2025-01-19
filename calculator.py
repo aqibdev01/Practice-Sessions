4e4180e48bee6eba0d9e1c117baffd458371d8d6
Aqib Abdullah <aqibabdullahdev@gmail.com>
2025-01-19 11:32:27 +0500

    Help Me God

--- calculator.py
+++ calculator.py
@@ -12,25 +12,49 @@ def multiplication(m1,m2):
 def division(v1,v2):
     print(v1/v2)
 
-a1 = int(input("Enter the first number to add: "))
-a2 = int(input("Enter the second number to add: "))
+# a1 = int(input("Enter the first number to add: "))
+# a2 = int(input("Enter the second number to add: "))
 
-sum(a1,a2)
-
-
-b1 = int(input("Enter the first number to subtract: "))
-b2 = int(input("Enter the first number to subtract: "))
-
-difference(b1,b2)
+# sum(a1,a2)
 
 
-c1 = int(input("Enter the first number to multiply: "))
-c2 = int(input("Enter the first number to multiply: "))
+# b1 = int(input("Enter the first number to subtract: "))
+# b2 = int(input("Enter the first number to subtract: "))
 
-multiplication(c1,c2)
+# difference(b1,b2)
 
-d1 = int(input("Enter the first number to divide: "))
-d2 = int(input("Enter the first number to divide: "))
 
-division(d1,d2)
+# c1 = int(input("Enter the first number to multiply: "))
+# c2 = int(input("Enter the first number to multiply: "))
 
+# multiplication(c1,c2)
+
+# d1 = int(input("Enter the first number to divide: "))
+# d2 = int(input("Enter the first number to divide: "))
+
+# division(d1,d2)
+
+def check_operation(opt, opd1, opd2):
+    if opt=='+':
+        a = sum(opd1,opd2)
+    elif opt=='-':
+        a = difference(opd1,opd2)
+    elif opt=='*':
+        a = multiplication(opd1, opd2)
+    elif opt=='/':
+        a = division(opd1,opd2)
+
+def starts():
+    opd1 = input("Enter first value: ")
+    opd2 = input("Enter second value: ")
+
+    opt = input("Which operation you want to perform? (+,-,*,/) ")
+
+    check_operation(opt, opd1, opd2)
+
+    x = input("Do you want to perform another operation on resultant? (y/n) ")
+    if x.lower() == 'y':
+        opt = input("Which operation you want to perform? (+,-,*,/) ")
+        opd1 = input("Enter the value: ")
+
+starts()
