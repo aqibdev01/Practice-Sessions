aa8f4f98200fc5db58d61698d061e3a45351d340
Aqib Abdullah <aqibabdullahdev@gmail.com>
2025-01-19 10:39:22 +0500

    Modular Calculator w/ static values

--- calculator.py
+++ calculator.py
@@ -1,1 +1,18 @@
 print("Welcome to Calculator")
+
+def sum(s1,s2):
+    return s1+s2
+
+def difference(d1,d2):
+    return d1-d2
+
+def multiplication(m1,m2):
+    return m1*m2
+
+def division(v1,v2):
+    return v1,v2
+
+sum(1,2)
+difference(5,2)
+multiplication(3,2)
+division(9,3)
