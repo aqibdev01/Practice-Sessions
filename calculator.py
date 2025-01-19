db516422b0ca4eb33d99730178052446b0320798
Aqib Abdullah <aqibabdullahdev@gmail.com>
2025-01-19 10:48:23 +0500

    Corrections made

--- calculator.py
+++ calculator.py
@@ -1,18 +1,19 @@
 print("Welcome to Calculator")
 
 def sum(s1,s2):
-    return s1+s2
+    print(s1+s2)
 
 def difference(d1,d2):
-    return d1-d2
+    print(d1-d2)
 
 def multiplication(m1,m2):
-    return m1*m2
+    print(m1*m2)
 
 def division(v1,v2):
-    return v1,v2
+    print(v1/v2)
 
 sum(1,2)
 difference(5,2)
 multiplication(3,2)
 division(9,3)
+
