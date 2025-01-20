a733a82adb25c765c45d21a5a2987efb188b0026
Aqib Abdullah <aqibabdullahdev@gmail.com>
2025-01-20 23:00:00 +0500

    Table of n

--- loops.py
+++ loops.py
@@ -1,27 +1,37 @@
-print("Showing Even and Odd numbers seperately")
+def even_odd():
+    print("Showing Even and Odd numbers seperately")
 
-count_five = 0
-count_ten = 0
-for i in range(1, 101):
-    if i%2 == 0:
+    count_five = 0
+    count_ten = 0
+    for i in range(1, 101):
+        if i%2 == 0:
+            if i%5 == 0:
+                count_five += 1
+            if i%10 == 0:
+                count_ten += 1
+            print(f"{i} is even")
+        else:
+            if i%5 == 0:
+                count_five += 1
+            if i%10 == 0:
+                count_ten += 1
+            print(f"{i} is odd")
+
+    print(f"multiples of 5: {count_five}")
+    print(f"multiples of 10: {count_ten}")
+
+    for i in range(1, 100):
         if i%5 == 0:
-            count_five += 1
+            print(f"{i} is multiple of 5")
+        
         if i%10 == 0:
-            count_ten += 1
-        print(f"{i} is even")
-    else:
-        if i%5 == 0:
-            count_five += 1
-        if i%10 == 0:
-            count_ten += 1
-        print(f"{i} is odd")
+            print(f"{i} is multiple of 10")
 
-print(f"multiples of 5: {count_five}")
-print(f"multiples of 10: {count_ten}")
 
-for i in range(1, 100):
-    if i%5 == 0:
-        print(f"{i} is multiple of 5")
-    
-    if i%10 == 0:
-        print(f"{i} is multiple of 10")
+def table_of():
+    n = int(input("Enter a number: "))
+    for i in range(1, 11):
+        t = n*i
+        print(f"{n} x {i} = {t}")
+
+table_of()
