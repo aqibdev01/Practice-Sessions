b47e4b08423cae46bfd1eeb282db7561f8dcb031
Aqib Abdullah <aqibabdullahdev@gmail.com>
2025-01-21 09:07:31 +0500

    Refactored with user based output

--- en-de coding.py
+++ en-de coding.py
@@ -1,32 +1,38 @@
+def encode(to_encode):
+    encoded_array = []
+    for i in range(len(to_encode)):
+        ascii_value = ord(to_encode[i])
+        encoded = ascii_value + 1
+        encoded_array.insert(i, encoded)
+    return encoded_array
 
-
-def encode(x):
-    c = []
-    for i in range(len(x)):
-        a = x[i]
-        b = ord(a)
-        b += 1
-        c.insert(i, b)
-    return c 
-
-def decode(y):
-    z = []
-    for i in range(len(y)):
-        a = y[i]
-        a -= 1
-        b = chr(a)
-        z.insert(i, b)
-    return z
+def decode(to_decode):
+    decoded_array = []
+    for i in range(len(to_decode)):
+        encoded_value = to_decode[i]
+        in_ascii = encoded_value - 1
+        decoded = chr(in_ascii)
+        decoded_array.insert(i, decoded)
+    return decoded_array
 
 def start():
 
     string = input("Enter a line: ")
     encoded = encode(string)
-
-    decoded = decode(encoded)
-
     print(encoded)
-    print(decoded)
+
+    choice_for_decode = input("Do you want to decode this output? (y/n) ")
+    if choice_for_decode.lower() == 'y':
+        decoded = decode(encoded)
+        print(decoded)
+    else:
+        pass
+
+    another_try = input("Do you want to encode another string? (y/n) ")
+    if another_try == 'y':
+        start()
+    else:
+        print("Bye")
 
 
 start()
